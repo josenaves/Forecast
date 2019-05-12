6b00c59567bd9ce2f473eab970bb2335703965c5
José Naves <josenaves@gmail.com>
2019-05-12 10:08:16 -0300

    feat: Add DarkSky API mapping

---
+++ app/src/main/java/com/josenaves/forecast/data/remote/api/DarkSkyApi.kt
@@ -0,0 +1,19 @@
+package com.josenaves.forecast.data.remote.api
+
+import com.josenaves.forecast.data.remote.api.dto.ForecastResponseDto
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+interface DarkSkyApi {
+
+    @Headers("Content-Type: application/json")
+    @GET("/forecast/2bb07c3bece89caf533ac9a5d23d8417/{latitude},{longitude}")
+    fun getForecastAsync(
+        @Path("latitude") latitude: Double,
+        @Path("longitude") longitude: Double
+    ): Deferred<Response<ForecastResponseDto>>
+
+}
---
+++ app/src/main/java/com/josenaves/forecast/data/remote/api/dto/ForecastResponseDto.kt
@@ -0,0 +1,28 @@
+package com.josenaves.forecast.data.remote.api.dto
+
+data class ForecastResponseDto(
+    val latitude: Double,
+    val longitude: Double,
+    val timezone: String,
+    val currently: CurrentForecast
+)
+
+data class CurrentForecast(
+    val time: Long,
+    val summary: String,
+    val icon: String,
+    val precipIntensity: Double,
+    val precipProbability: Double,
+    val temperature: Double,
+    val apparentTemperature: Double,
+    val dewPoint: Double,
+    val humidity: Double,
+    val pressure: Double,
+    val windSpeed: Double,
+    val windGust: Double,
+    val windBearing: Int,
+    val cloudCover: Double,
+    val uvIndex: Double,
+    val visibility: Double,
+    val ozone: Double
+)
